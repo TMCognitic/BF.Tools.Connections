4cafbfcec1ce23d3a7882148a8306a3471723f8d
Morre Thierry <thierry.morre@cognitic.be>
2022-05-03 11:17:28 +0200

    Ajoutez des fichiers projet.

---
+++ Tools.Connections/Connection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Connections
+{
+    public class Connection
+    {
+        private readonly string _connectionString;
+
+        public Connection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int ExecuteNonQuery(Command command)
+        {
+            using (SqlConnection dbConnection = CreateConnection())
+            {
+                using(SqlCommand dbCommand = CreateCommand(dbConnection, command))
+                {
+                    dbConnection.Open();
+                    return dbCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public IEnumerable<TResult> ExecuteReader<TResult>(Command command, Func<IDataRecord, TResult> selector)
+        {
+            using (SqlConnection dbConnection = CreateConnection())
+            {
+                using (SqlCommand dbCommand = CreateCommand(dbConnection, command))
+                {
+                    dbConnection.Open();
+                    using(SqlDataReader reader = dbCommand.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            yield return selector(reader);
+                        }
+                    }
+                }
+            }
+        }
+
+        private SqlCommand CreateCommand(SqlConnection dbConnection, Command command)
+        {
+            SqlCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = command.Query;
+            if(command.IsStoredProcedure)
+                dbCommand.CommandType = CommandType.StoredProcedure;
+
+            foreach (KeyValuePair<string, object> kvp in command.Parameters)
+            {
+                SqlParameter dbParameter = dbCommand.CreateParameter();
+                dbParameter.ParameterName = kvp.Key;
+                dbParameter.Value = kvp.Value;
+                dbCommand.Parameters.Add(dbParameter);
+            }
+
+            return dbCommand;
+        }
+
+        private SqlConnection CreateConnection()
+        {
+            SqlConnection dbConnection = new SqlConnection();
+            dbConnection.ConnectionString = _connectionString;
+            return dbConnection;
+        }
+    }
+}
